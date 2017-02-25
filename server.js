9a7516f3d10bff9d1bc68250e3782e6280a8ea7e
Simón García <garciarodriguez.simon@outlook.com>
2017-02-25 10:20:29 +0100

    correccion del puerto de escucha

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ io.on('connection', function (socket) {
 
 
 //Pone el servidor a escuchar el puerto 3000
-http.listen(3000, function () {
+http.listen(process.env.PORT || 3000, function () {
     console.log('listening on *:3000');
 });
 
